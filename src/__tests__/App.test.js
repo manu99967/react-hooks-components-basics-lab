69f7971ae206d045ec18a2550b64a04e676017b6
Emmanuel <www.proxynova95@gmail.com>
2025-07-20 12:39:11 +0300

    update

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,8 @@
+// JavaScript
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-
 import App from "../components/App";
+import React from "react";
 
 test("renders without errors", () => {
   expect(() => render(<App />)).not.toThrow();
@@ -9,13 +10,6 @@ test("renders without errors", () => {
 
 test("renders the correct child components", () => {
   const { container } = render(<App />);
-
-  /*
-    Uncomment the line below to see the DOM elements being returned
-    by the App component in your terminal when you run the tests
-  */
-  // screen.debug();
-
   expect(container.querySelector("nav")).toBeInTheDocument();
   expect(container.querySelector("#home")).toBeInTheDocument();
   expect(container.querySelector("#about")).toBeInTheDocument();
--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,11 @@
 import React from "react";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-{/* write an <About> component here */}
-
-
 function App() {
   return (
     <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <nav>Navigation</nav>
+      <div id="home">Home Content</div>
+      <div id="about">About Content</div>
     </div>
   );
 }
